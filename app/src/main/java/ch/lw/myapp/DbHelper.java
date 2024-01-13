1f3164d46b57d5d52d3051a65465c595043d0d9b
Leaali <ehcw2004@gmail.com>
2024-01-13 16:41:08 +0100

    feat - delete Semester

--- app/src/main/java/ch/lw/myapp/DbHelper.java
+++ app/src/main/java/ch/lw/myapp/DbHelper.java
@@ -76,4 +76,14 @@ public class DbHelper extends SQLiteOpenHelper {
 
     }
 
+    void deleteOneRow(String row_id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(TABLE_NAME, "_id=?", new String[]{row_id});
+        if(result == -1){
+            Toast.makeText(context, "Fehler, das Semester konnte nicht gelöscht werden.", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Erfolgreich gelöscht.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/ch/lw/myapp/UpdateSemesterActivity.java
+++ app/src/main/java/ch/lw/myapp/UpdateSemesterActivity.java
@@ -1,17 +1,17 @@
 package ch.lw.myapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class UpdateSemesterActivity extends AppCompatActivity {
     EditText input_title_edit;
-    Button  button_update_edit;
+    Button  button_update_edit, button_delete_edit;
     String id, title;
 
     @Override
@@ -21,30 +21,43 @@ public class UpdateSemesterActivity extends AppCompatActivity {
 
         input_title_edit = findViewById(R.id.input_title_edit);
         button_update_edit = findViewById(R.id.button_update_edit);
+        button_delete_edit = findViewById(R.id.button_delete_edit);
 
         getAndSetIntentData();
 
-        button_update_edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DbHelper helperDB = new DbHelper(UpdateSemesterActivity.this);
-                title = input_title_edit.getText().toString().trim();
-                helperDB.updateData(id, title);
-            }
-        });
-
+        button_update_edit.setOnClickListener(view -> updateData());
+        button_delete_edit.setOnClickListener(view -> confirmDialog());
     }
-    void getAndSetIntentData(){
-        if(getIntent().hasExtra("id") && getIntent().hasExtra("title")){
-            //Getting Data from Intent
+    void getAndSetIntentData() {
+        if (getIntent().hasExtra("id") && getIntent().hasExtra("title")) {
             id = getIntent().getStringExtra("id");
             title = getIntent().getStringExtra("title");
-
-            //Setting Intent Data
             input_title_edit.setText(title);
             Log.d("stev", title);
-        }else{
+        } else {
             Toast.makeText(this, "Keine Daten.", Toast.LENGTH_SHORT).show();
         }
     }
+
+    void updateData() {
+        String newTitle = input_title_edit.getText().toString().trim();
+        if (!newTitle.isEmpty()) {
+            new DbHelper(UpdateSemesterActivity.this).updateData(id, newTitle);
+        } else {
+            Toast.makeText(this, "Titel darf nicht leer sein.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void confirmDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Semester " + title + " löschen?")
+                .setMessage("Bist du dir sicher, dass du das Semester " + title + " löschen möchtest?")
+                .setPositiveButton("Ja", (dialogInterface, i) -> {
+                    new DbHelper(UpdateSemesterActivity.this).deleteOneRow(id);
+                    finish();
+                })
+                .setNegativeButton("Nein", null)
+                .show();
+    }
+
 }
